c276d6a8844cef3eb2fecaa47a118b8a96f84a2f
codenarok <atasert.mert@gmail.com>
2025-03-27 13:35:37 +0000

    Add SearchFunction and related files

---
+++ api/Program.cs
@@ -0,0 +1,10 @@
+using Microsoft.Azure.Functions.Worker.Extensions.OpenApi.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(services => { })
+    .Build();
+
+host.Run();
